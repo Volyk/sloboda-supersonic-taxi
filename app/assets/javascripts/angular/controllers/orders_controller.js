b868222dc106b5bef93731d91965248876a72621
yelizaveta-dziuba <elizabeth.dzyuba@gmail.com>
2016-10-10 14:51:27 +0300

    layouts for drivers and dispatchers login pages

--- app/assets/javascripts/angular/controllers/orders_controller.js
+++ app/assets/javascripts/angular/controllers/orders_controller.js
@@ -1,24 +1,7 @@
 (function() {
-    var app = angular.module('taxi', ['ngRoute']);
+var app = angular.module('taxi', ['ngRoute']);
 
-/*
-    app.controller('CreateOrderController',['$scope', '$http', function($scope, $http) {
-        $scope.order = {};
-        $scope.phone_pattern = /(0)[0-9]{9}/;
-        $scope.email_pattern = /^(([-a-z0-9._]+)@([-a-z]+))(\.[a-z]+)?(\.[a-z]+)?$/;
-
-        $scope.addOrder = function () {
-            console.log($scope.order);
-            $http.post('/orders', $scope.order).success(function(data){
-                console.log(data);
-                $scope.order = {};
-            })
-        }
-
-    }]);
-*/
-
-app.controller('CreateOrderController', function($scope, $http) {
+app.controller('CreateOrderController', ['$scope', '$http', function($scope, $http) {
     $scope.order = {};
         $scope.phone_pattern = /(0)[0-9]{9}/;
         $scope.email_pattern = /^(([-a-z0-9._]+)@([-a-z]+))(\.[a-z]+)?(\.[a-z]+)?$/;
@@ -35,29 +18,48 @@ app.controller('CreateOrderController', function($scope, $http) {
         'comment'   : $scope.order.comment
       }
     };
-
-    $http({
-        method: 'POST',
-        url: 'http://localhost:3000/orders',
-    data: $scope.order
+    $http.post('/orders', $scope.order).success(function(data){
+        $scope.order = {};
     });
-};
+    };
+}]);
 
+app.controller('DriversController', ['$scope', function($scope) {
+    $scope.text = 'Номер телефона';
+    $scope.length = 10;
+    $scope.placeholder = '0987654321';
+    
+
+}]);
+
+app.controller('DispatchersController', ['$scope', function($scope) {
+    $scope.text = 'Логин';
+    $scope.length = 20;
+    $scope.placeholder = 'example';
+}]);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true); 
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/home.html',
+            controller: 'CreateOrderController'
+        })
+        .when('/driver', {
+            templateUrl: 'templates/login.html',
+            controller: 'DriversController'
+        })
+        .when('/dispatcher', {
+            templateUrl: 'templates/login.html',
+            controller: 'DispatchersController'
+        })
+}]);
+
+app.directive("navigationPanel", function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/navbar.html'
+    }
 });
-
-    app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'templates/home.html',
-                controller: 'CreateOrderController'
-            })
-    }]);
-
-    app.directive("navigationPanel", function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/navbar.html'
-        }
-    })
 
 }) ();
