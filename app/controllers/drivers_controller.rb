cbe2589bd2ff59b2dd10f400b94cbfe39bac24e5
SudakovEugene <SudakovEugene@gmail.com>
2016-10-15 17:31:07 +0300

    Add validates for active drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,6 +2,5 @@ class DriversController < ApplicationController
 before_action :authenticate_driver!, only: [:profile]	
 
   def profile
- 
   end
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,5 +4,10 @@ class Driver < ApplicationRecord
   devise :database_authenticatable, :rememberable, :trackable, authentication_keys: [:phone]
   validates :phone, presence: true, uniqueness: true
   validates_format_of :phone, with: /\A(0)([0-9]{9})\z/
+  
+  def active_for_authentication?
+  	super && active?
+  end	
+	
 end
 
