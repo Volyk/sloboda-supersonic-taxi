03b058b683ae6a1cb747f4ee804f3f32ecea84e4
SudakovEugene <SudakovEugene@gmail.com>
2016-10-15 22:03:49 +0300

    Fix application

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,8 @@
 class Order < ApplicationRecord
-	validates_format_of :email, with: /\A(.+)@(.+)\z/
 	validates :end_point, :passengers, :start_point, presence: true
 	validates :phone, presence: true
 	validates_format_of :phone, with: /\A(0)([0-9]{9})\z/
 	validates :baggage, presence: true, allow_blank: false
 
   	has_many :orders_blogs
-  	belongs_to :driver
 end
