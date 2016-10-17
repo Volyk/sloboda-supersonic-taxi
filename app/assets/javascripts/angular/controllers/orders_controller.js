599750bd26f579a2637c1785eeedfe4dd7be0bd4
yelizaveta-dziuba <elizabeth.dzyuba@gmail.com>
2016-10-17 17:29:33 +0300

    driver interface ui

--- app/assets/javascripts/angular/controllers/orders_controller.js
+++ app/assets/javascripts/angular/controllers/orders_controller.js
@@ -5,70 +5,70 @@ app.controller('CreateOrderController', ['$scope', '$http', function($scope, $ht
     $scope.phone_pattern = /(0)[0-9]{9}/;
     $scope.email_pattern = /^(.+)@(.+)$/;
     $scope.addOrder = function() {
-    // if (!$scope.order.email) {
-    // $scope.order.email = '';
-    // }
-    $http.post('/orders', $scope.order).success(function(data){
-        alert('Ваш заказ принят!');
-        $scope.order = {};
-    });
+      if (!$scope.order.email) {
+        $scope.order.email = '';
+      }
+      $scope.order.email = $scope.order.email.toLowerCase();      
+      $http.post('/orders', $scope.order).success(function(data){
+          alert('Ваш заказ принят!');
+          $scope.order = {};
+      });
     };
 }]);
 
 
 app.controller('DriversController', ['$scope', '$http', function($scope, $http) {
-  $scope.empty = true;
 
   $http.get('/drivers/orders.json').success(function(data){
     $scope.orders = data;
-    angular.forEach($scope.orders, function(order, key) {
-      order.waiting = true;
-    })
-    if ($scope.orders[0]) { 
-      $scope.empty = false;
-    }
   });
 
   $scope.deleteOrder = function(order) {
     var index = $scope.orders.indexOf(order);
     $scope.orders.splice(index, 1);
-    if ($scope.orders[0]) { 
-      $scope.empty = false;
-    } else {
-      $scope.empty = true;
-    } 
-  }
+  };
+
+  $scope.putMethod = function(data) {
+    var url = '/orders/' + data.id;
+    $http.put(url, data);
+  };
 
   $scope.acceptOrder = function(order) {
     order.waiting = false;
     order.accepted = true;
-    $http.patch('/drivers/orders', order);
+    $scope.putMethod(order);
   };
 
   $scope.declineOrder = function(order) {
     order.waiting = false;
     order.declined = true;
-    $http.patch('/drivers/orders', order);
+    $scope.putMethod(order);
     $scope.deleteOrder(order);    
   };
 
   $scope.arrivedToOrder = function(order) {
     order.accepted = false;
     order.arrived = true;
-    $http.patch('/drivers/orders', order);
+    $scope.putMethod(order);
   };
 
   $scope.orderFulfilled = function(order) {
     order.arrived = false;
     order.done = true;
-    $http.patch('/drivers/orders', order);
+    $scope.putMethod(order);
     $scope.deleteOrder(order);
   };
 
 }]);
 
 app.controller('DispatchersController', ['$scope', '$http', function($scope, $http) {
-  //will get and show data in template
+
+  $http.get('/dispatchers/orders.json').success(function(data){
+    $scope.orders = data;
+  });
+
+
+
 }]);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
