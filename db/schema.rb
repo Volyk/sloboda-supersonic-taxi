509ba7c6ae43601c285a623304e1e305074aac70
SerjB <bi4hiji@mail.ru>
2016-10-18 12:04:51 +0300

    returned sample files

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,19 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20161011133401) do
-  enable_extension 'plpgsql '
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
     t.string   "login",               default: "", null: false
@@ -14,8 +27,7 @@ ActiveRecord::Schema.define(version: 20161011133401) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "active"
-    t.index ["login"], name: "index_admins_on_login",
-                       unique: true, using: :btree
+    t.index ["login"], name: "index_admins_on_login", unique: true, using: :btree
   end
 
   create_table "dispatchers", force: :cascade do |t|
@@ -32,11 +44,8 @@ ActiveRecord::Schema.define(version: 20161011133401) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "active"
-    t.index ["email"], name: "index_dispatchers_on_email",
-                       unique: true, using: :btree
-    t.index ["reset_password_token"],
-            name: "index_dispatchers_on_reset_password_token",
-            unique: true, using: :btree
+    t.index ["email"], name: "index_dispatchers_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_dispatchers_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "drivers", force: :cascade do |t|
@@ -57,11 +66,8 @@ ActiveRecord::Schema.define(version: 20161011133401) do
     t.string   "last_sign_in_ip"
     t.boolean  "active"
     t.string   "name"
-    t.index ["phone"], name: "index_drivers_on_phone",
-                       unique: true, using: :btree
-    t.index ["reset_password_token"],
-            name: "index_drivers_on_reset_password_token",
-            unique: true, using: :btree
+    t.index ["phone"], name: "index_drivers_on_phone", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_drivers_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "orders", force: :cascade do |t|
@@ -87,4 +93,34 @@ ActiveRecord::Schema.define(version: 20161011133401) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
 end
