d8216838e9466817606a174fa08498b36589d004
yelizaveta-dziuba <elizabeth.dzyuba@gmail.com>
2016-10-18 16:15:01 +0300

    fixed hash keys

--- app/assets/javascripts/angular/controllers/orders_controller.js
+++ app/assets/javascripts/angular/controllers/orders_controller.js
@@ -4,10 +4,10 @@ var app = angular.module('taxi', ['ngRoute', 'ngDialog']);
 app.controller('CreateOrderController', ['$scope', '$http', function($scope, $http) {
   $scope.data = {
     availableOptions: [
-      {id: '1'}, {id: '2'}, {id: '3'}, {id: '4'}, 
-      {id: '5'}, {id: '6'}, {id: '7'}, {id: '8'}
+      {value: '1'}, {value: '2'}, {value: '3'}, {value: '4'}, 
+      {value: '5'}, {value: '6'}, {value: '7'}, {value: '8'}
     ],
-    selectedOption: {id: '1'}
+    selectedOption: {value: '1'}
   };
   $scope.phone_pattern = /(0)[0-9]{9}/;
   $scope.email_pattern = /^(.+)@(.+)$/;
@@ -16,13 +16,13 @@ app.controller('CreateOrderController', ['$scope', '$http', function($scope, $ht
       $scope.order.email = '';
     }
     $scope.order.email = $scope.order.email.toLowerCase();
-    $scope.order.passengers = $scope.data.selectedOption.id;
+    $scope.order.passengers = $scope.data.selectedOption.value;
     $http.post('/orders', $scope.order).success(function(data){
       alert('Ваш заказ принят!');
       $scope.order = {};
     });
   };
-  
+
 }]);
 
 
