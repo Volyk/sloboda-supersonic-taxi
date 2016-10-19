a5a5cf432cef8f446fee7a1c1bb0a68a7e8d4648
Andrii Volyk <volyk.andrii@gmail.com>
2016-10-19 19:59:46 +0300

    Factory

--- spec/factories/drivers.rb
+++ spec/factories/drivers.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :driver do
-    phone '0' + Faker::Number.number(9)
-    password Faker::Internet.password(8)
+    phone { '0' + Faker::Number.number(9) }
+    password { Faker::Internet.password(8) }
     active true
   end
 end
