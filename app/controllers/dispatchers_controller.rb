47d070f3d28ed174574adc2555fea2fe42b1d580
Andrii Volyk <volyk.andrii@gmail.com>
2016-10-20 18:44:54 +0300

    Add Controllers for dispatcher

--- app/controllers/dispatchers_controller.rb
+++ app/controllers/dispatchers_controller.rb
@@ -1,8 +1,26 @@
+# Dispatchers controller
 class DispatchersController < ApplicationController
-	before_action :authenticate_dispatcher!, only: [:profile]	
+  before_action :authenticate_dispatcher!
+  respond_to :html, :json
 
   def profile
- 
+
   end
-  
+
+  def orders
+    @orders = Order.where(status: %w(incoming declined))
+    respond_with(@orders) do |format|
+      format.json { render json: @orders.as_json }
+      format.html
+    end
+  end
+
+  def drivers
+    @drivers = Driver.where(active: true)
+    respond_with(@drivers) do |format|
+      format.json { render json: @drivers.as_json }
+      format.html
+    end
+  end
+
 end
