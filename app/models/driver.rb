8048b94533ad0dfc3e58aab8070ec52ece855cf5
SerjB <bi4hiji@mail.ru>
2016-10-20 15:13:30 +0300

    commit by request

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,7 +2,8 @@
 class Driver < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable, authentication_keys: [:phone]
+  devise :database_authenticatable, :rememberable,
+         :trackable, authentication_keys: [:phone]
   validates :phone, presence: true, uniqueness: true
   validates_format_of :phone, with: /\A(0)([0-9]{9})\z/
 
