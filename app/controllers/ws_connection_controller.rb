039c692073fab60fd55e1476e55d34faed733d5a
Andrii Volyk <volyk.andrii@gmail.com>
2016-10-21 03:21:19 +0300

    Bring this thing to life

--- app/controllers/ws_connection_controller.rb
+++ app/controllers/ws_connection_controller.rb
@@ -6,17 +6,23 @@ class WsConnectionController < WebsocketRails::BaseController
   end
 
   def client_connected
-    return if current_driver.nil?
-    WebsocketRails.users[current_driver.id] = connection
-    @orders = Order.where(status: %w(waiting arrived accepted)) \
-                   .where(driver_id: current_driver.id)
-    current_driver.status = @orders.empty? ? 'available' : 'busy'
-    current_driver.save
+    if !current_dispatcher.nil?
+      @name = 'disp' + current_dispatcher.id.to_s
+      WebsocketRails.users[@name] = connection
+    elsif !current_driver.nil?
+      WebsocketRails.users[current_driver.id] = connection
+      @orders = Order.where(status: %w(waiting arrived accepted)) \
+                     .where(driver_id: current_driver.id)
+      current_driver.status = @orders.empty? ? 'available' : 'busy'
+      current_driver.save
+      broadcast_message :get_drivers, 'id' => current_driver.id
+    end
   end
 
   def delete_user
     return if current_driver.nil?
     current_driver.status = 'offline'
     current_driver.save
+    broadcast_message :get_drivers, 'id' => current_driver.id
   end
 end
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,5 +4,9 @@ require_relative '../../lib/tasks/orders_task'
 scheduler = Rufus::Scheduler.new
 order = OrdersTask.new
 scheduler.every '10s' do
-  order.time_check
+  if order.time_check != false
+    scheduler.in '5s' do
+      order.ws_warn
+    end
+  end
 end
